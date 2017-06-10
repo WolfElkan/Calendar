156a6d662a9b7d3aee28a58bc9c37506d69bec6f
Wolf Elkan <wolf.elkan@gmail.com>
2017-06-10 16:47:22 -0400

    Calendar headings responsive to window width

--- client/assets/Event/index/IndexEvent.js
+++ client/assets/Event/index/IndexEvent.js
@@ -1,4 +1,5 @@
-app.controller('IndexEvent',['$scope','$location','EventFactory',function($scope,$location,EventFactory) {
+app.controller('IndexEvent',['$','$scope','$location','EventFactory',
+	                function( $ , $scope , $location , EventFactory) {
 
 	EventFactory.get(function(content) {
 		$scope.events = content
@@ -8,24 +9,53 @@ app.controller('IndexEvent',['$scope','$location','EventFactory',function($scope
 		}
 	})
 
-	var timeW = 35
+	// $.foo()
+
+	function px(num) {
+		return '' + num + 'px'
+	}
+
+	var timeW = 40
 	var scrollBarW = 15
 
-	window.onresize = function() {
+	var day_head = document.getElementsByClassName('day-head')
+	var calendar = document.getElementsByClassName('calendar')
+	var time_sep = document.getElementsByClassName('time-sep')
 
-		var calW = window.innerWidth * 0.95 - scrollBarW - timeW
+	var rwd = function() {
 
-		$scope.day_head = {
-			width: calW / 7
-		}
-		$scope.calendar = {
-			width: calW
-		}
-		$scope.time_sep = {
-			width: timeW
-		}
+		// console.log('resize')
+
+		var calW = window.innerWidth * 0.95 - scrollBarW 
+		var dayW = px((calW - timeW) / 7)
+
+		day_head[0].style.width = dayW
+		day_head[1].style.width = dayW
+		day_head[2].style.width = dayW
+		day_head[3].style.width = dayW
+		day_head[4].style.width = dayW
+		day_head[5].style.width = dayW
+		day_head[6].style.width = dayW
+
+		calendar[0].style.width = px(calW)
+
+		time_sep[0].style.width = px(timeW)
+
+		// $scope.day_head = {
+		// 	width: calW / 7
+		// }
+		// $scope.calendar = {
+		// 	width: calW
+		// }
+		// $scope.time_sep = {
+		// 	width: timeW
+		// }
 		
 	}
+
+	rwd()
+
+	window.onresize = rwd
 
 	$scope.delete = function(id) {
 		EventFactory.delete(id)
